141f1c5f327e42ca60e7adfb7ab308501b0704d0
bekku(Hiroo Bekku) <65069410+bekku@users.noreply.github.com>
2022-06-03 04:33:20 +0900

    Update settings.py

--- PLBSCF_VISUALIZE/PLBSCF_VISUALIZE/settings.py
+++ PLBSCF_VISUALIZE/PLBSCF_VISUALIZE/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j%o18r_wsy+yp^&40_m1+-z=hq9%k4ocr2((85em@)#umwy41)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
